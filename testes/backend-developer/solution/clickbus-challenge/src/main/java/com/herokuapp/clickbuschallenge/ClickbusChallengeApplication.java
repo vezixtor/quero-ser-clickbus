9f588bb514a149b1dc478fba640a50f30b31b448
Everson <everson___@hotmail.com>
2019-08-13 00:37:18 -0300

    first commit

---
+++ testes/backend-developer/solution/clickbus-challenge/src/main/java/com/herokuapp/clickbuschallenge/ClickbusChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.herokuapp.clickbuschallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickbusChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickbusChallengeApplication.class, args);
+	}
+
+}
